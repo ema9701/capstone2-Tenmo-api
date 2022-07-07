15359cd06e7a06a6db0979b15dbc2f5fbab8bceb
Robert Weeks <rpweeks_2@yahoo.com>
2022-07-07 09:38:28 -0400

    committing database table changes

--- database/tenmo.sql
+++ database/tenmo.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS tenmo_user, account;
+DROP TABLE IF EXISTS tenmo_user, account, transactions, transaction_account;
 
 DROP SEQUENCE IF EXISTS seq_user_id, seq_account_id;
 
@@ -33,5 +33,22 @@ CREATE TABLE account (
 	CONSTRAINT FK_account_tenmo_user FOREIGN KEY (user_id) REFERENCES tenmo_user (user_id)
 );
 
+CREATE TABLE transactions (
+	transaction_id serial NOT NULL,
+	amount decimal (13, 2) NOT NULL,
+	is_requesting boolean NOT NULL,
+	status varchar (10) NOT NULL DEFAULT 'True',
+	CONSTRAINT PK_transactions PRIMARY KEY (transaction_id)
+);
+
+CREATE TABLE transaction_account (
+	transaction_id serial NOT NULL,
+	account_id_out int NOT NULL,
+	account_id_in int NOT NULL,
+	CONSTRAINT PK_transaction_account PRIMARY KEY (transaction_id),
+	CONSTRAINT FK_transaction_account FOREIGN KEY (transaction_id) REFERENCES transactions (transaction_id),
+	CONSTRAINT FK_transaction_account_out FOREIGN KEY (account_id_out) REFERENCES account (account_id),
+	CONSTRAINT FK_transaction_account_in FOREIGN KEY (account_id_in) REFERENCES account (account_id)
+);
 
 COMMIT;
